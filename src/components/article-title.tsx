b38baf54684a96f46bbbd2a5be971657f78b2866
euxn23 <euxn23@gmail.com>
2020-12-25 04:13:27 +0900

    fix: change days display

--- src/components/article-title.tsx
+++ src/components/article-title.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import dayjs from 'dayjs';
-import { monthNames } from '../helper/month-names';
 import { normalizePaperTitle } from '../helper';
 import { parseTitle } from '../helper/perser';
 import { CategoryTag } from './category-tag';
@@ -11,24 +10,26 @@ type Props = {
 };
 
 const colors = [
-  'bg-red-300',
   'bg-orange-300',
-  'bg-yellow-300',
-  'bg-green-300',
+  'bg-amber-300',
+  'bg-red-300',
   'bg-blue-300',
-  'bg-indigo-300',
+  'bg-green-300',
+  'bg-yellow-300',
   'bg-purple-300'
 ];
+
+const dayJaList = ['日', '月', '火', '水', '木', '金', '土']
 
 export function ArticleTitle({ entry }: Props) {
   const titleObject = parseTitle(normalizePaperTitle(entry.name));
   const { title, date: yyyymmdd, meta } = titleObject;
   const postedAt = dayjs(`${yyyymmdd || '20200101'}`);
 
-  const monthName = monthNames[postedAt.month()];
-  const date = postedAt.date();
+  const year = postedAt.year()
+  const date = postedAt.format('MM/DD');
   const day = postedAt.day();
-  const dateColor = colors[day % 7];
+  const dateColor = colors[day];
 
   return (
     <div className='flex w-full shadow rounded bg-white m-2 relative'>
@@ -37,8 +38,9 @@ export function ArticleTitle({ entry }: Props) {
           className={`flex justify-around w-full ${dateColor} rounded-sm py-4 block shadow-inner`}
         >
           <div className='text-center tracking-wide'>
-            <div className='text-white font-bold text-4xl'>{date}</div>
-            <div className='text-white font-normal text-2xl'>{monthName}</div>
+            <div className='text-white font-light text-sm'>{year}</div>
+            <div className='text-white font-bold text-2xl'>{date}</div>
+            <div className='text-white font-normal text-md'>{dayJaList[day]}</div>
           </div>
         </div>
       </div>
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  theme: {
+    colors: {
+      white: colors.white,
+      black: colors.black,
+      gray: colors.coolGray,
+      red: colors.red,
+      yellow: colors.yellow,
+      green: colors.green,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      purple: colors.purple,
+      pink: colors.pink,
+      orange: colors.orange,
+      amber: colors.amber
+    }
+  }
+}
