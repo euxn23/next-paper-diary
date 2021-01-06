517b4c45f5a1fad804c895c3a5b8c1a3a31c0ad5
euxn23 <euxn23@gmail.com>
2021-01-06 12:40:18 +0900

    fix: use NEXT_PUBLIC env

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 export const hostname = 'https://blog.euxn.me'
 export const appImage = 'https://github.com/euxn23.png'
-export const appTitle = process.env.APP_TITLE ?? ''
+export const appTitle = process.env.NEXT_PUBLIC_APP_TITLE ?? ''
